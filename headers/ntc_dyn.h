d10daeee15749cdf7ae880c68475ce22630ef631
svitlana1209 <righoor8@gmail.com>
2023-06-21 11:46:24 +0300

    Update dyn-info

--- headers/ntc_dyn.h
+++ headers/ntc_dyn.h
@@ -1,5 +1,6 @@
 void get_dynamic_info(hashtable *, dyn_struct *);
 void display_dynamic_info(FILE *, dyn_struct *, total *);
 HashKey * get_max_vol(hashtable *);
+HashKey * get_pair_for_max(hashtable *, HashKey *);
 
 
--- ntc_dyn.c
+++ ntc_dyn.c
@@ -10,16 +10,24 @@ void get_dynamic_info(hashtable *ht, dyn_struct *dyn) {
 HashKey *hkey, *hkey_pair;
 
     hkey = get_max_vol(ht);
-    dyn->src_x  = hkey->srcIP;
-    dyn->dst_y  = hkey->dstIP;
-    dyn->vol1   = hkey->vol;
-    dyn->packs1 = hkey->packs;
+    dyn.src_x  = hkey->srcIP;
+    dyn.dst_y  = hkey->dstIP;
+    dyn.vol1   = hkey->vol;
+    dyn.packs1 = hkey->packs;
 
     hkey_pair = get_pair_for_max(ht, hkey);
-    dyn->src_y  = hkey_pair->srcIP;
-    dyn->dst_x  = hkey_pair->dstIP;
-    dyn->vol2   = hkey_pair->vol;
-    dyn->packs2 = hkey_pair->packs;
+    if (hkey_pair) {
+        dyn.src_y  = hkey_pair->srcIP;
+        dyn.dst_x  = hkey_pair->dstIP;
+        dyn.vol2   = hkey_pair->vol;
+        dyn.packs2 = hkey_pair->packs;
+    }
+    else {
+        dyn.src_y  = 0;
+        dyn.dst_x  = 0;
+        dyn.vol2   = 0;
+        dyn.packs2 = 0;
+    }
 }
 
 HashKey * get_max_vol(hashtable *ht_head) {
@@ -43,3 +51,20 @@ hashtable *ht;
     return hkey_max;
 }
 
+HashKey * get_pair_for_max(hashtable *ht_head, HashKey *hkey_max) {
+HashKey *hkey;
+hashtable *ht;
+
+    ht = ht_head;
+    while (ht) {
+        hkey = ht->ptr_to_hashkey;
+        while (hkey) {
+            if ((hkey-srcIP == hkey_max->dstIP) && (hkey-dstIP == hkey_max->srcIP)) {
+                return hkey;
+            }
+            hkey = hkey->next;
+        }
+        ht = ht->next;
+    }
+    return NULL;
+}
